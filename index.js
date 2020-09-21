c77e3eb3e37370a94a470e35ff9d93e715e9e3eb
lunexout <69036038+lunexout@users.noreply.github.com>
2020-09-21 11:04:13 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 let geo_language = document.getElementById("geo_language");
 let usa_language = document.getElementById("usa_language");
-let k = 0;
+// let k = 0;
 
-function language_change() {
-  if (k == 0) {
-    geo_language.style.display = "none";
-    usa_language.style.display = "block";
-    k++;
-  } else if (k == 1) {
-    usa_language.style.display = "none";
-    geo_language.style.display = "block";
-    k--;
-  }
+// function language_change() {
+//   if (k == 0) {
+//     geo_language.style.display = "none";
+//     usa_language.style.display = "block";
+//     k++;
+//   } else if (k == 1) {
+//     usa_language.style.display = "none";
+//     geo_language.style.display = "block";
+//     k--;
+//   }
+// }
+
+
+// geo_language.addEventListener("click", language_change);
+// usa_language.addEventListener("click", language_change);
+
+
+function eng_index_html() {
+  window.open("eng_index.html" , "_self");
 }
 
-geo_language.addEventListener("click", language_change);
-usa_language.addEventListener("click", language_change);
-
+function geo_index_html() {
+  window.open("index.html" , "_self");
+}
 // background javascript
 //end
 
